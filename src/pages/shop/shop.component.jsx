bf9bb5b953f00c9896c8533413c8871b139d45d6
Michael Brink <michael.brink@hotmail.com>
2019-08-13 21:03:01 +1000

    added add item and remove item functionality to checkout item component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import SHOP_DATA from './shop.data.js';
 
-import CollectionPreview from  '../../components/collection-preview/collection-preview.component';
+import CollectionPreview from  '../../components/collection-preview/collection-preview';
 
 class ShopPage extends React.Component {
     constructor(props) {
@@ -19,10 +19,9 @@ class ShopPage extends React.Component {
             {
                 collections.map(({id, ...otherCollectionProps }) => (
                     <CollectionPreview key={id} {...otherCollectionProps} />
-                ))
-            }
-
-        </div>); 
+                ))}
+        </div>
+        ); 
     }
 }
 
